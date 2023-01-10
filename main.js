17d30e7d512f73d170a5b0be0974207a24d44a49
Aryan <AryanEstaralie@swibe.de>
2023-01-10 17:52:19 +0100

    header changes

--- main.js
+++ main.js
@@ -1,8 +1,26 @@
-const body = document.querySelector("body");
-const icon = document.querySelector(".theme-icon");
+let body = document.querySelector("body");
+let icon = document.querySelector(".theme-icon");
+let header = document.querySelector(".header-main")
+let mainSection = document.querySelector("main")
+
 
 icon.onclick = function () {
     body.classList.toggle("dark-theme");
     icon.classList.toggle("bi-moon-stars");
     icon.classList.toggle("bi-sun");
 };
+
+// mainSection.addEventListener("scroll",  () => {
+//     console.log(mainSection.scrollY)
+//     if (mainSection.scrollY > 0) {
+//         document.querySelector("header").classList.add("nav-scrolled");
+//     } else {
+//         document.querySelector("header").classList.remove("nav-scrolled");
+//     }
+// });
+
+// mainSection.onscroll = function () {
+//     if (mainSection.scrollY > 0) {
+//         header.classList.toggle("on-scroll")
+//     }
+// }
